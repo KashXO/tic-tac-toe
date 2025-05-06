5e524a9d11adbe6e9540174740175e9e6ff64010
KashXO <kazimarzuk25@gmail.com>
2025-05-07 03:54:14 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import TicTacToe from './components/tictactoe';
+
+function App() {
+
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App
